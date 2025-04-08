991db514c3457179fb7c25d31b7a93da11d6678a
SanketLV <sanketlakhani80@gmail.com>
2025-04-08 20:11:31 +0530

    added landing page and upload page

---
+++ app/upload/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { FileUpload } from "@/components/ui/file-upload";
+import React, { useState } from "react";
+
+const UploadPage = () => {
+  const [files, setFiles] = useState<File[]>([]);
+  const handleFileUpload = (files: File[]) => {
+    setFiles(files);
+    console.log(files);
+  };
+
+  return (
+    <div className="flex items-center w-full h-screen border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg">
+      <FileUpload onChange={handleFileUpload} />
+    </div>
+  );
+};
+
+export default UploadPage;
